2be96687bed6d592b7efc413c8866e8816de2c87
Lasha Khoriashvili <LkhoriashviliOfficial@gmail.com>
2020-12-18 18:41:52 +0400

    Added News Properties page

---
+++ SixtyThreeBits.Web/wwwroot/admin/js/News/NewsItem.js
@@ -0,0 +1,13 @@
+﻿$(function () {
+    TinyMCE.Init({
+        Selector: '.js-apply-tinymce'
+    }).Display();
+
+    $('.js-slug-textbox').change(function () {
+        $(this).val($(this).ToSlug());
+    });
+
+    $('.js-save-button').click(function () {
+        preloader.show();
+    });
+});
